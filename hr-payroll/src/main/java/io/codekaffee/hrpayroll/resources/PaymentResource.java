49d92531f506d4f24b77485126c94c567dc54cb4
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-03-18 18:06:33 -0300

    Hystrix configured

--- hr-payroll/src/main/java/io/codekaffee/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/io/codekaffee/hrpayroll/resources/PaymentResource.java
@@ -1,5 +1,7 @@
 package io.codekaffee.hrpayroll.resources;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +20,16 @@ public class PaymentResource {
     private PaymentService paymentService;
 
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getWorkerPayment(@PathVariable("workerId") Long workerId, @PathVariable("days") Integer days){ 
         Payment workerPayment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(workerPayment);
+    }
+
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Brann", 400.00, days);
+        return ResponseEntity.ok(payment);
     }
 }
